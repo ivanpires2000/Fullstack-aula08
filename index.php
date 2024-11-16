10093a6249ddbe753b5590a1affa663f7ed6918d
Ivan Pires da Silva Ferreira <ivan.pires2000@gmail.com>
2024-11-16 11:53:46 +0000

    Introdução ao backend

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aula 08</title>
+</head>
+<body>
+    <h1>Introdução a programação backend</h1>
+
+    <?php
+
+        echo 'Olá, Ivan';
+        echo '<br>';
+
+        //Cria uma variavel chamada ano_nascimento
+        $ano_nascimento = 1983;
+
+        //Cria uma variavel chamada ano_atual
+        $ano_atual = 2024;
+
+        //Criamos uma variavel para armazenar a idade da pessoa,
+        //Realizamos um calculo de subtração entre o ano atual eo ano de nascimento
+        $idade = $ano_atual - $ano_nascimento;
+
+        //Exibimos a idade da pessoa
+        //No php, o caracter "." concatena (junta) strings.
+        echo 'Voce possui ' . $idade . ' anos.';
+    ?>
+
+    <br>
+    
+    <a href="formulario.php">Ir para o formulário</a>
+
+
+
+
+</body>
+</html>
